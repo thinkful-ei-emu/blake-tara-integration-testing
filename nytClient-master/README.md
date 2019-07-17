b1774209b765815eb53a7c6e144e631022ac3589
Tara Patel <t.jeanne.patel@gmail.com>
2019-07-17 15:08:05 -0500

    added playstore app

--- nytClient-master/README.md
+++
@@ -1,6 +0,0 @@
-# NYT Bestseller Client
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-
-_This is a demonstration project. It is the React client for the NYT Server built with Express_
--- nytClient-master/src/book/book.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Book from './book';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Book />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
